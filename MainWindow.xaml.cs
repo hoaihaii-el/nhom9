e2f1aa630b12dab6eb130643034414d0fe8d3ec6
El Nino <hoaihai.el@gmail.com>
2022-09-13 21:52:16 +0700

    add code

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RestaurantManagementSystem
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnWareHouse_Click(object sender, RoutedEventArgs e)
+        {
+            MainMenu.Content = new WareHouse();
+        }
+
+        private void btnStaff_Click(object sender, RoutedEventArgs e)
+        {
+            MainMenu.Content = new Staff();
+        }
+
+        private void btnMenu_Click(object sender, RoutedEventArgs e)
+        {
+            MainMenu.Content = new Menu();
+        }
+
+        private void btnTable_Click(object sender, RoutedEventArgs e)
+        {
+            MainMenu.Content = new Table();
+        }
+
+        private void btnBill_Click(object sender, RoutedEventArgs e)
+        {
+            MainMenu.Content = new Bill();
+        }
+
+        private void btnStats_Click(object sender, RoutedEventArgs e)
+        {
+            MainMenu.Content = new Stats();
+        }
+
+        private void btnViewProfile_Click(object sender, RoutedEventArgs e)
+        {
+            Profile objViewProfile = new Profile();
+            objViewProfile.Show();
+        }
+    }
+}
---
+++ Profile.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.IO;
+
+namespace RestaurantManagementSystem
+{
+    /// <summary>
+    /// Interaction logic for Profile.xaml
+    /// </summary>
+    public partial class Profile : Window
+    {
+        public Profile()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnChangeAvatar_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == true)
+            {
+                Uri fileUri = new Uri(ofd.FileName);
+                avatar.Source = new BitmapImage(fileUri);
+            }
+        }
+    }
+}
